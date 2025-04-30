15837ed3d3f71b0f37a92fcbd72f4ad60bb1924b
Galih Aditya <113104415+mid-aditya@users.noreply.github.com>
2025-04-30 09:02:18 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="google-site-verification" content="kode_verifikasi_panjang_di_sini" />
+    <meta name="google-site-verification" content="VrCE8j8LFZq8nIuRavE8qYxFmYag2j7um571qK5kzCQ" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PUSDIKZI TNI AD</title>
     <link rel="icon" type="image/png" href="/assets/images/logo-zeni.png">
